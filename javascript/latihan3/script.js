e3cda89eae66d2cdb53da7e22b908c4177602f0a
flx <fstarrail222@gmail.com>
2024-04-16 16:55:16 +0700

    Pertemuan 13

--- javascript/latihan3/script.js
+++ javascript/latihan3/script.js
@@ -1,10 +1,46 @@
-let nama = document.getElementById("nama")
-let testimoni = document.getElementById("testimoni")
-console.log(nama.value)
+let inputNama = document.getElementById("nama");
+let inputTestimoni = document.getElementById("testimoni");
+let btnSimpan = document.getElementById("btnSimpan");
+let list = document.getElementById("list");
 
 let data = []
-function simpan(){
-    data.push(nama.value, testimoni.value);
-    console.log(data)
 
+btnSimpan.addEventListener("click", function(){
+    if(localStorage.getItem("testimoni")){
+        data = JSON.parse(localStorage.getItem("testimoni"));
+        data.push({
+            nama : inputNama.value,
+            testimoni : inputTestimoni.value,
+        })
+    } else {
+        data.push({
+            nama : inputNama.value,
+            testimoni : inputTestimoni.value,
+        })    
+    }
+
+    localStorage.setItem("testimoni", JSON.stringify(data));
+
+    getData();
+
+})
+
+// tampilkan data localstorage
+function getData(){
+    if(localStorage.getItem("testimoni")){
+        // clear element list
+        list.innerHTML = "";
+        // ada localstorage dengan key testimoni
+        data = JSON.parse(localStorage.getItem("testimoni"));
+        for (const [index, row] of data.entries()){
+            list.innerHTML += `
+                    <tr>
+                    <td>${row.nama}</td>
+                    <td>${row.testimoni}</td>
+                    </tr>
+            `;
+        }
+    }
 }
+
+getData();
